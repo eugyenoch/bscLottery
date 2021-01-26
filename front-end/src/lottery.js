b8b60d2e920b25692760be3a5bdc3550d81a4e15
Eugy Enoch <38564978+eugyenoch@users.noreply.github.com>
2021-01-26 11:49:09 +0100

    Create lottery.js

---
+++ front-end/src/lottery.js
@@ -0,0 +1,44 @@
+import web3 from "./web3";
+
+const address = "0x9EA8d6272B724AfB813A9E7C4eaF07C7aF4FE996";
+const abi = [
+    {
+        "inputs": [
+            {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            },
+            {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "name": "getRandom",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "name": "randomNumber",
+        "outputs": [
+            {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    }
+]
+
+export default new web3.eth.Contract(abi, address);
